329398ef9e5a8ff9137d5fa0f77228a1efe326bc
413831 <alepleins3pp@gmail.com>
2020-07-19 17:27:01 -0300

    Fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  middleware_url: 'https://spotify-middleware.herokuapp.com/spotify',
+  client_id: '27634a95ab0f46c688c28483a2790188',
+  client_secret: '17addf11fa0d45ad95e0f917d09ca006',
 };
